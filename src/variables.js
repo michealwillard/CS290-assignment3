5e49d6db0f12e8277bcc036f5cf85384402de5f7
michealwillard <willardm@onid.oregonstate.edu>
2015-01-24 19:16:58 -0800

    Completed variables.js, updated log.txt

--- src/variables.js
+++ src/variables.js
@@ -14,7 +14,9 @@ function variableModification(a) {
   var asString;
   var yourNumberIs;
   //your code here
-
+  plus5 = 5 + a;
+  asString = String(a);
+  yourNumberIs = 'Your Number is ' + a;
   //end your code
   return [plus5, asString, yourNumberIs, a];
 }
@@ -29,7 +31,12 @@ false otherwise
 */
 function isString(b) {
   //your code here
-  return undefined;
+  if (typeof(b) === 'string') {
+    return true;
+  }
+  else {
+    return false;
+  }
   //end your code
 }
 
